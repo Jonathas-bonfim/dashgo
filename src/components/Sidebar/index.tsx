f6490871ee947fbcea7ebd61648754f5722027d6
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2022-06-16 13:41:54 -0300

    componentizatized sidebar

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,30 @@
+import { Box, Stack } from "@chakra-ui/react";
+import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
+import { NavLinkProps } from "./NavLink";
+import { NavSection } from "./NavSection";
+
+
+export function SideBar() {
+  return (
+    <Box
+      as="aside"
+      w="64"
+      mr="8"
+    >
+      <Stack
+        spacing="12"
+        align="flex-start"
+      >
+        <NavSection title="Geral">
+          <NavLinkProps icon={RiDashboardLine}>Dashboard</NavLinkProps>
+          <NavLinkProps icon={RiContactsLine}>Usuários</NavLinkProps>
+        </NavSection>
+        <NavSection title="AUTOMAÇÃO">
+          <NavLinkProps icon={RiInputMethodLine}>Formulários</NavLinkProps>
+          <NavLinkProps icon={RiGitMergeLine}>Automação</NavLinkProps>
+        </NavSection>
+
+      </Stack>
+    </Box>
+  )
+}
