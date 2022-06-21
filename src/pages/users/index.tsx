b5e12867006b1d7c41aa0f364256433963e935ed
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2022-06-21 19:27:29 -0300

    dynamic pages react

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
+import { useState } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -8,7 +9,8 @@ import { useUsers } from "../../services/useUsers";
 
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useUsers();
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -67,7 +69,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map(user => {
+                  {data.users.map(user => {
                     return (
                       <Tr key={user.id}>
                         <Td px={["4", "4", "6"]}>
@@ -96,7 +98,7 @@ export default function UserList() {
                   })}
                 </Tbody>
               </Table>
-              <Pagination totalContOfRegisters={200} currentPage={5} onPageChange={() => { }} />
+              <Pagination totalContOfRegisters={data.totalCount} currentPage={page} onPageChange={setPage} />
             </>
           }
         </Box>
