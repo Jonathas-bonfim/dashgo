81ff6616da9884b73105db4e915dce9a90f07a54
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2022-06-21 16:01:38 -0300

    useUsers

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,35 +1,14 @@
-import { useEffect } from "react";
-import { useQuery } from 'react-query';
-
-import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue, useRangeSlider } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from '../../components/Sidebar'
-import { api } from "../../services/api";
+import { useUsers } from "../../services/useUsers";
 
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const { data } = await api.get('/users')
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-br', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        }),
-      }
-    });
-
-    return users;
-  }, {
-    staleTime: 1000 * 5,
-  });
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
